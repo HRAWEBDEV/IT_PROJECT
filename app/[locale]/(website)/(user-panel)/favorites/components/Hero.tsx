5c77b7f1a292dbe4e11e762d09f4f6558ff41a5c
hamidwise1 <hrawebdev@gmail.com>
2025-03-07 15:08:49 +0330

    update images

--- app/[locale]/(website)/(user-panel)/favorites/components/Hero.tsx
+++ app/[locale]/(website)/(user-panel)/favorites/components/Hero.tsx
@@ -1,9 +1,9 @@
 export default function Hero() {
  return (
-  <section className='bg-neutral-200 dark:bg-neutral-800 h-44 lg:h-56 mb-8'>
-   <div className='container flex flex-col justify-center h-full'>
-    <div className='text-center font-bold text-2xl lg:text-3xl text-red-500'>
-     علاقه‌منــــدی‌ها
+  <section className='mb-8 grid justify-center pt-10'>
+   <div className='grid'>
+    <div className='text-center font-bold text-2xl lg:text-3xl text-red-500 dakr:text-red-600'>
+     علاقه‌مندی‌هــا
     </div>
    </div>
   </section>
