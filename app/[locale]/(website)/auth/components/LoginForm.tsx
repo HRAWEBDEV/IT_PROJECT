0d738290e5cf8d3a0c63a409ad0182ce6da17f4e
hamidwise1 <hrawebdev@gmail.com>
2025-04-11 12:23:10 +0330

    update login page

--- app/[locale]/(website)/auth/components/LoginForm.tsx
+++ app/[locale]/(website)/auth/components/LoginForm.tsx
@@ -7,43 +7,42 @@ import { GradientButton } from '@/components/Button/GradientButton';
 export default function LoginForm() {
  return (
   <div className='container'>
-   <form className='my-5 lg:my-20 rounded-lg border border-neutral-300 dark:border-neutral-700 p-4'>
-    <div className='flex items-center justify-center text-2xl text-primary h-[8rem] bg-teal-50 dark:bg-teal-950'>
-     LOGO
-    </div>
-    <div className='flex items-center gap-2 mb-14'>
-     <span className='flex-grow h-[1px] bg-foreground'></span>
-     <span className='text-base font-medium'>ورود</span>
-     <span className='flex-grow h-[1px] bg-foreground'></span>
-    </div>
-    <div className='mb-12'>
-     <div className='grid gap-6'>
-      <TextField fullWidth label='نام کاربری' required />
-      <TextField fullWidth label='رمز عبور' required />
-     </div>
-    </div>
-    <div className='flex justify-between'>
-     <div className='flex flex-col gap-4'>
-      <Button
-       variant='outlined'
-       LinkComponent={Link}
-       href='#'
-       color='secondary'
-       className='w-[7rem]'
-      >
-       ثبت نام
-      </Button>
-      <Button
-       LinkComponent={Link}
-       href='#'
-       color='error'
-       className='!text-[0.75rem]'
-      >
-       فراموشی رمز؟
-      </Button>
-     </div>
+   <form className='my-5 lg:my-20 rounded-xl bg-gradient-to-br from-sky-600 to-teal-400 dark:from-sky-400 dark:to-teal-300'>
+    <div className='text-primary-foreground flex justify-between items-center mb-4 p-4'>
      <div>
-      <GradientButton className='w-[8rem]'>تایید</GradientButton>
+      <p className='font-medium text-2xl'>ورود</p>
+     </div>
+     <div className='bg-primary-foreground aspect-square w-[5rem] rounded-lg text-foreground grid place-content-center font-medium'>
+      LOGO
+     </div>
+    </div>
+    <div className='bg-primary-foreground rounded-xl border-[1px] border-neutral-300 shadow-md p-6 pt-10'>
+     <div className='grid gap-8 mb-10'>
+      <TextField fullWidth size='small' label='نام کاربری' required />
+      <TextField fullWidth size='small' label='پسوورد' required />
+     </div>
+     <div className='flex justify-between'>
+      <div className='grid gap-2'>
+       <Button
+        LinkComponent={Link}
+        variant='outlined'
+        className='w-[6rem]'
+        href='#'
+       >
+        ثبت نام
+       </Button>
+       <Button
+        color='error'
+        LinkComponent={Link}
+        className='w-[6rem] !text-[0.7rem]'
+        href='#'
+       >
+        فراموشی رمز؟
+       </Button>
+      </div>
+      <div>
+       <GradientButton className='w-[8rem]'>تایید</GradientButton>
+      </div>
      </div>
     </div>
    </form>
