cb63e467285d9cb0ca7615ed21b643b23f8d9d7f
hamidwise1 <hrawebdev@gmail.com>
2025-04-11 20:39:55 +0330

    update login scrol

--- app/[locale]/(website)/auth/layout.tsx
+++ app/[locale]/(website)/auth/layout.tsx
@@ -3,9 +3,9 @@ import AuthTabs from './components/AuthTabs';
 
 export default function layout({ children }: PropsWithChildren) {
  return (
-  <div className='h-[calc(100vh_-_var(--header-height)_-_var(--mobile-bottom-nav-height))] lg:h-[calc(100vh_-_var(--header-height))] bg-neutral-200 dark:bg-neutral-800'>
-   <div className='py-8 px-4 h-full'>
-    <div className='container h-full bg-background rounded-xl grid lg:grid-cols-2 px-0'>
+  <div className='h-[calc(100vh_-_var(--header-height)_-_var(--mobile-bottom-nav-height))] lg:h-[calc(100vh_-_var(--header-height))] overflow-hidden bg-neutral-200 dark:bg-neutral-800'>
+   <div className='py-8 px-4 h-full overflow-hidden'>
+    <div className='container h-full bg-background rounded-xl grid lg:grid-cols-2 px-0 overflow-auto'>
      <div>
       <AuthTabs />
       {children}
