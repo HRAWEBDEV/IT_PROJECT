8ddf196f2666f2e1f8fe73026e4744975bda0876
hamidwise1 <hrawebdev@gmail.com>
2025-04-22 19:27:48 +0330

    add services section animation

--- app/[locale]/(website)/components/services/Services.tsx
+++ app/[locale]/(website)/components/services/Services.tsx
@@ -11,6 +11,7 @@ import StorageIcon from '@mui/icons-material/Storage';
 import WifiIcon from '@mui/icons-material/Wifi';
 import SecurityCameraIcon from '@/components/icons/SecurityCameraIcon';
 import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import { motion } from 'motion/react';
 
 const iconSize = '2.5rem';
 const projects = [
@@ -44,7 +45,24 @@ export default function Services() {
    <div>
     <div className='container text-center mb-8'>
      <div className='pb-2 mb-2 relative after:content-[""] before:content-[""] after:absolute after:start-[50%] after:bottom-0 after:w-[10rem] after:translate-x-[50%] after:h-[4px] after:bg-neutral-400 before:absolute before:start-[50%] before:bottom-[1px] before:w-[15rem] before:translate-x-[50%] before:h-[2px] before:bg-neutral-400 after:rounded-3xl before:rounded-3xl'>
-      <h2 className='text-2xl font-bold lg:text-3xl'>خدمــــات</h2>
+      <motion.h1
+       initial={{
+        y: -10,
+        opacity: 0,
+       }}
+       whileInView={{
+        y: 0,
+        opacity: 1,
+       }}
+       viewport={{ amount: 'some' }}
+       transition={{
+        duration: 0.5,
+        ease: 'easeInOut',
+       }}
+       className='text-2xl font-bold lg:text-3xl'
+      >
+       خدمــــات
+      </motion.h1>
      </div>
      <p className='text-neutral-500 dark:text-neutral-200 w-[min(100%,40rem)] text-center leading-7 mb-10 container lg:text-base lg:leading-7'>
       لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از
@@ -55,7 +73,20 @@ export default function Services() {
     <div className='container mx-auto w-[min(100%,60rem)] mb-6'>
      <ul className='grid md:grid-cols-2'>
       {projects.map((item, i) => (
-       <li key={item.title}>
+       <motion.li
+        initial={{
+         x: i % 2 == 0 ? 100 : -100,
+        }}
+        whileInView={{
+         x: 0,
+        }}
+        viewport={{ amount: 'some' }}
+        transition={{
+         duration: 0.5,
+         ease: 'easeInOut',
+        }}
+        key={item.title}
+       >
         <Link
          className='flex gap-4 p-6 transition-colors hover:bg-neutral-200 focus:bg-neutral-200 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 rounded-lg'
          href='#'
@@ -77,7 +108,7 @@ export default function Services() {
           </p>
          </div>
         </Link>
-       </li>
+       </motion.li>
       ))}
      </ul>
     </div>
