f1c379807b21149ff2484e6db4b1bed983ff0755
hamidwise1 <hrawebdev@gmail.com>
2025-04-25 09:52:20 +0330

    update banner color

--- app/[locale]/(website)/co-services/[name]/components/Banner.tsx
+++ app/[locale]/(website)/co-services/[name]/components/Banner.tsx
@@ -5,11 +5,11 @@ export default function Banner() {
  return (
   <section className='h-[12rem] lg:h-[17rem] bg-[url(/images/services/fiber.jpg)] bg-cover bg-center'>
    <div className='h-full flex flex-col justify-center bg-gradient-to-b from-black/20 to-black/65'>
-    <h1 className='text-center font-bold text-4xl lg:text-6xl text-background mb-8  bg-cover bg-center'>
+    <h1 className='text-center font-bold text-4xl lg:text-6xl text-primary-foreground mb-8  bg-cover bg-center'>
      فیبر نـــوری
     </h1>
-    <div className='flex justify-center text-background'>
-     <Breadcrumbs className='!text-background'>
+    <div className='flex justify-center text-primary-foreground'>
+     <Breadcrumbs className='!text-primary-foreground'>
       <Link href='/'>خانه</Link>
       <Link href='/co-services'>خدمات</Link>
       <Link href='/services/fiber-optic'>فیبر نوری</Link>
