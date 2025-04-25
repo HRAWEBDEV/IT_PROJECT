0f781f41a41f9f18bb45479e7e29be18aab9918c
hamidwise1 <hrawebdev@gmail.com>
2025-04-25 11:53:32 +0330

    add servicse comment section

--- app/[locale]/(website)/co-services/[name]/page.tsx
+++ app/[locale]/(website)/co-services/[name]/page.tsx
@@ -1,11 +1,13 @@
 import WhyUs from './components/WhyUs';
 import Content from './components/Content';
+import CommentSection from './components/comments/CommentSection';
 
 export default function page() {
  return (
   <section>
    <Content />
    <WhyUs />
+   <CommentSection />
   </section>
  );
 }
