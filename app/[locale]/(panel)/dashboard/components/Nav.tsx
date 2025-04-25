c56a3175b17e47e60655a33e08c6b94da854fe93
hamidwise1 <hrawebdev@gmail.com>
2025-04-25 15:55:42 +0330

    update project dashboard

--- app/[locale]/(panel)/dashboard/components/Nav.tsx
+++ app/[locale]/(panel)/dashboard/components/Nav.tsx
@@ -1,6 +1,12 @@
 'use client';
 import { useAppMonitorConfig } from '@/services/app-monitor/appMonitor';
 import { useNavigationContext } from '../navigation/navigationContext';
+import SettingsIcon from '@mui/icons-material/Settings';
+import HandymanIcon from '@mui/icons-material/Handyman';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import Link from 'next/link';
+
+const iconsStyle = { fontSize: '2rem' };
 
 export default function Nav() {
  const { isNavigationVisible } = useNavigationContext();
@@ -18,7 +24,52 @@ export default function Nav() {
   <nav
    className={`fixed transition-transform ${navPosition} top-[--dashboard-header-height] lg:block bottom-0 w-[--dashboard-nav-width] bg-background shadow-lg border-e border-neutral-300 dark:border-neutral-700`}
   >
-   test
+   <ul>
+    <li>
+     <Link
+      href='/dashboard/setting'
+      className='flex transition-colors items-center gap-4 p-4 min-h-[4.5rem] text-base font-medium border-b border-neutral-300 dark:border-neutral-700 hover:bg-neutral-100 dark:hover:bg-neutral-900'
+     >
+      <div>
+       <SettingsIcon sx={iconsStyle} />
+      </div>
+      <span>تنظیمات</span>
+     </Link>
+    </li>
+    <li>
+     <Link
+      href='/dashboard/services'
+      className='flex transition-colors items-center gap-4 p-4 min-h-[4.5rem] text-base font-medium border-b border-neutral-300 dark:border-neutral-700 hover:bg-neutral-100 dark:hover:bg-neutral-900'
+     >
+      <div>
+       <HandymanIcon sx={iconsStyle} />
+      </div>
+      <span>خدمات</span>
+     </Link>
+     <li>
+      <Link
+       href='/dashboard/services'
+       className='flex transition-colors items-center gap-4 p-4 min-h-[4.5rem] text-base font-medium border-b border-neutral-300 dark:border-neutral-700 hover:bg-neutral-100 dark:hover:bg-neutral-900'
+      >
+       <div>
+        <HandymanIcon sx={iconsStyle} />
+       </div>
+       <span>پروژ‌هـــا</span>
+      </Link>
+     </li>
+    </li>
+    <li>
+     <Link
+      href='/dashboard/services'
+      className='flex transition-colors items-center gap-4 p-4 min-h-[4.5rem] text-base font-medium border-b border-neutral-300 dark:border-neutral-700 hover:bg-neutral-100 dark:hover:bg-neutral-900'
+     >
+      <div>
+       <NewspaperIcon sx={iconsStyle} />
+      </div>
+      <span>اخبار و مقــاله‌ها</span>
+     </Link>
+    </li>
+   </ul>
   </nav>
  );
 }
