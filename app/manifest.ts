deb7caf69148bab5b756ff707eff8002689875e9
hamidwise1 <hrawebdev@gmail.com>
2025-04-27 14:09:10 +0330

    add manifest file

---
+++ app/manifest.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from 'next';
+
+const manifest = (): MetadataRoute.Manifest => {
+ return {
+  theme_color: '#007aff',
+  background_color: '#ffffff',
+  icons: [
+   {
+    purpose: 'maskable',
+    sizes: '512x512',
+    src: 'favs/icon512_maskable.png',
+    type: 'image/png',
+   },
+   {
+    purpose: 'any',
+    sizes: '512x512',
+    src: 'favs/icon512_rounded.png',
+    type: 'image/png',
+   },
+  ],
+  orientation: 'any',
+  display: 'standalone',
+  dir: 'auto',
+  lang: 'fa-IR',
+  name: 'نرم افزار آیتی نترا',
+  short_name: 'آیتی نترا',
+  start_url: '/',
+  scope: '.',
+ };
+};
+
+export default manifest;
