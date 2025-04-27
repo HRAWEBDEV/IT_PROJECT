e79ef353bff56410a64fca1e7800cfe4b4af1527
hamidwise1 <hrawebdev@gmail.com>
2025-04-27 14:16:19 +0330

    update next config to remove logs

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
  eslint: {
   ignoreDuringBuilds: true,
  },
+ compiler: {
+  removeConsole: process.env.NODE_ENV === 'production',
+ },
 };
 
 export default nextConfig;
