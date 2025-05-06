a2243639f204a6a4240a04d62c84629da07182ab
hamidwise1 <hrawebdev@gmail.com>
2025-05-06 20:37:55 +0330

    update toaster position

--- services/notifications/ToastrProvider.tsx
+++ services/notifications/ToastrProvider.tsx
@@ -1,23 +1,16 @@
 'use client';
-import IconButton from '@mui/material/IconButton';
 import { ReactNode, FC } from 'react';
-import { SnackbarProvider, closeSnackbar } from 'notistack';
-import CloseIcon from '@mui/icons-material/Close';
-import { useTheme } from '@mui/material/styles';
+import { SnackbarProvider } from 'notistack';
 
 type TProps = { children: ReactNode };
 
 const ToastrProvider: FC<TProps> = ({ children }) => {
- const matTheme = useTheme();
- const snackbarHorizontalOrigin =
-  matTheme.direction === 'rtl' ? 'left' : 'right';
-
  return (
   <SnackbarProvider
    autoHideDuration={2000}
    anchorOrigin={{
     vertical: 'top',
-    horizontal: snackbarHorizontalOrigin,
+    horizontal: 'center',
    }}
    hideIconVariant
    preventDuplicate={true}
