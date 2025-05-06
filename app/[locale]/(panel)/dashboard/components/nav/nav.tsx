cc5546dd34db04c7ce9f2c4b831279d196ca1de5
hamidwise1 <hrawebdev@gmail.com>
2025-05-06 12:13:13 +0330

    update nav border

--- app/[locale]/(panel)/dashboard/components/nav/nav.tsx
+++ app/[locale]/(panel)/dashboard/components/nav/nav.tsx
@@ -5,7 +5,7 @@ import { navigationList } from '../../utils/navgationList';
 
 export default function nav() {
  return (
-  <nav className='w-[--dashboard-nav-width] border-l border-neutral-300 dark:border-neutral-700 py-4 overflow-auto'>
+  <nav className='w-[--dashboard-nav-width] border-e border-neutral-300 dark:border-neutral-700 py-4 overflow-auto'>
    <NavList navigationList={navigationList} />
   </nav>
  );
