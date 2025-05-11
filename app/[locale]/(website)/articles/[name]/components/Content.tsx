918f8da3b0b1736a9325274563953e88295fa416
hamidwise1 <hrawebdev@gmail.com>
2025-05-11 12:33:11 +0330

    add articles ck content

--- app/[locale]/(website)/articles/[name]/components/Content.tsx
+++ app/[locale]/(website)/articles/[name]/components/Content.tsx
@@ -1,35 +1,15 @@
 'use client';
-import { useRef, useId } from 'react';
 import { type Blog } from '@/services/api-actions/globalApiActions';
-import { CKEditor as CKEditorType } from '@ckeditor/ckeditor5-react';
-import { ClassicEditor } from 'ckeditor5';
-
-import dynamic from 'next/dynamic';
-const ContentEditor = dynamic(
- () => import('@/components/ck-editor/ContentEditor'),
- {
-  ssr: false,
- }
-);
 
 type Props = {
  blog: Blog | null;
 };
 
 export default function Content({ blog }: Props) {
- const id = useId();
- const editorRef = useRef<CKEditorType<ClassicEditor>>(null);
  return (
-  <article className='my-12 container'>
-   <ContentEditor
-    ref={(ref) => {
-     editorRef.current = ref;
-     if (ref) {
-      ref.editor?.enableReadOnlyMode(id);
-     }
-    }}
-    data={blog?.body || ''}
-   />
-  </article>
+  <article
+   className='my-12 container ck-content'
+   dangerouslySetInnerHTML={{ __html: blog?.body || '' }}
+  ></article>
  );
 }
