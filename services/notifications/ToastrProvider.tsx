955b05cc62fd8bd1d56c30920b3c78803519e8a2
hamidwise1 <hrawebdev@gmail.com>
2025-05-15 16:21:29 +0330

    update toaster

--- services/notifications/ToastrProvider.tsx
+++ services/notifications/ToastrProvider.tsx
@@ -1,7 +1,8 @@
 'use client';
 import { ReactNode, FC } from 'react';
-import { SnackbarProvider } from 'notistack';
-
+import { SnackbarProvider, closeSnackbar } from 'notistack';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 type TProps = { children: ReactNode };
 
 const ToastrProvider: FC<TProps> = ({ children }) => {
@@ -15,10 +16,24 @@ const ToastrProvider: FC<TProps> = ({ children }) => {
    hideIconVariant
    preventDuplicate={true}
    maxSnack={8}
+   action={(snackbarId) => (
+    <div className='absolute end-0 top-[50%] translate-y-[-50%]'>
+     <IconButton
+      sx={{
+       color: 'inherit',
+      }}
+      onClick={() => closeSnackbar(snackbarId)}
+     >
+      <CloseIcon />
+     </IconButton>
+    </div>
+   )}
    style={{
     display: 'flex',
     justifyContent: 'space-between',
     gap: '1rem',
+    paddingInlineEnd: '2.5rem',
+    position: 'relative',
    }}
   >
    {children}
