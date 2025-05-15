2593fdd3684b7b56c3b531252a5575342b55024a
hamidwise1 <hrawebdev@gmail.com>
2025-05-15 17:55:38 +0330

    add auth provider

---
+++ services/auth/AuthProvider.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { useState, PropsWithChildren, useCallback, useMemo } from 'react';
+import {
+ authContext,
+ getAuthFromCookie,
+ setAuthToCookie,
+ logout,
+} from './authContext';
+
+export default function AuthProvider({ children }: PropsWithChildren) {
+ const [isLogedIn, setIsLogedIn] = useState(false);
+
+ //
+ const setAuthToken = useCallback((value: string) => {
+  setAuthToCookie(value);
+  setIsLogedIn(true);
+ }, []);
+ const getAuthToken = useCallback(() => {
+  return getAuthFromCookie();
+ }, []);
+ const removeAuthToken = useCallback(() => {
+  logout();
+  setIsLogedIn(false);
+ }, []);
+ //
+
+ const ctx = useMemo(() => {
+  return {
+   isLogedIn,
+   setAuthToken,
+   getAuthToken,
+   removeAuthToken,
+  };
+ }, [isLogedIn, setAuthToken, getAuthToken, removeAuthToken]);
+
+ return <authContext.Provider value={ctx}>{children}</authContext.Provider>;
+}
---
+++ services/auth/authContext.ts
@@ -0,0 +1,53 @@
+import { use, createContext } from 'react';
+import { setCookie, getCookie } from '@/utils/manageCookies';
+import { OutOfContext } from '@/utils/OutOfContext';
+
+//
+const authCookieName = 'auth';
+
+function getAuthFromCookie(): string | null {
+ const cookie = getCookie(authCookieName);
+ if (!cookie) return null;
+ return JSON.parse(cookie) as string;
+}
+
+function setAuthToCookie(value: string) {
+ setCookie({
+  name: authCookieName,
+  value,
+  expireDays: 30,
+ });
+}
+
+function logout() {
+ setCookie({
+  name: authCookieName,
+  value: '',
+  expireDays: 0,
+ });
+}
+//
+
+type Store = {
+ isLogedIn: boolean;
+ setAuthToken: (value: string) => void;
+ getAuthToken: () => string | null;
+ removeAuthToken: () => void;
+};
+
+const authContext = createContext<Store | null>(null);
+
+function useAuth() {
+ const auth = use(authContext);
+ if (!auth) throw new OutOfContext();
+ return auth;
+}
+
+export {
+ type Store,
+ useAuth,
+ authContext,
+ getAuthFromCookie,
+ setAuthToCookie,
+ logout,
+};
