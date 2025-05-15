b3e72199e4418dc015c2a2319f770de51eef281b
hamidwise1 <hrawebdev@gmail.com>
2025-05-15 21:26:05 +0330

    add request barear

--- services/axios/AxiosInterceptor.tsx
+++ services/axios/AxiosInterceptor.tsx
@@ -1,6 +1,22 @@
 'use client';
-import './axios';
+import { useEffect } from 'react';
+import { useAuth } from '../auth/authContext';
+import { axios } from './axios';
+import { useAppConfig } from '../app-config/appConfig';
 
 export default function AxiosInterceptor() {
+ const { locale } = useAppConfig();
+ const { getAuthToken } = useAuth();
+ useEffect(() => {
+  const cleanUp = () => {
+   axios.interceptors.request.eject(requestWatcher);
+  };
+  const requestWatcher = axios.interceptors.request.use((config) => {
+   config.headers.set('Authorization', `Bearer ${getAuthToken()}`);
+   config.headers.set('languageID', locale);
+   return config;
+  });
+  return cleanUp;
+ }, [getAuthToken, locale]);
  return <></>;
 }
