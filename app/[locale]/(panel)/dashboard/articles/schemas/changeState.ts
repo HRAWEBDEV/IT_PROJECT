74fb8ceea7e2b6d51415b7188663f77555771094
hamidwise1 <hrawebdev@gmail.com>
2025-05-16 17:22:07 +0330

    bug fix

--- app/[locale]/(panel)/dashboard/articles/schemas/changeState.ts
+++ app/[locale]/(panel)/dashboard/articles/schemas/changeState.ts
@@ -1,12 +1,10 @@
 import { z } from 'zod';
 
 const changeStateSchema = z.object({
- state: z
-  .object({
-   id: z.number(),
-   name: z.string(),
-  })
-  .nullable(),
+ state: z.object({
+  id: z.string(),
+  name: z.string(),
+ }),
 });
 
 type ChangeStateSchema = z.infer<typeof changeStateSchema>;
