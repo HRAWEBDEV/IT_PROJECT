919b13cbb3c5674396e5e0a8ba2630ac943d83ac
hamidwise1 <hrawebdev@gmail.com>
2025-05-25 16:57:15 +0330

    update user profile

--- app/[locale]/(website)/(user-panel)/profile/layout.tsx
+++ app/[locale]/(website)/(user-panel)/profile/layout.tsx
@@ -1,13 +1,17 @@
 import { PropsWithChildren } from 'react';
 import ProfileLayout from './components/ProfileLayout';
 import { Metadata } from 'next';
+import { AppParams } from '@/utils/appParams';
 
 export const metadata: Metadata = {
  title: 'آیتی نترا |‌ فناوری و اطلاعات | پروفایل',
 };
 
-const layout = ({ children }: PropsWithChildren) => {
- return <ProfileLayout>{children}</ProfileLayout>;
+const layout = async ({
+ children,
+ params,
+}: PropsWithChildren<{ params: Promise<AppParams> }>) => {
+ return <ProfileLayout params={params}>{children}</ProfileLayout>;
 };
 
 export default layout;
