c98f266517ca27295509fed579965619eee7b3e7
hamidwise1 <hrawebdev@gmail.com>
2025-05-26 20:21:21 +0330

    update article share button

--- app/[locale]/(website)/articles/components/articles/ArticlesList.tsx
+++ app/[locale]/(website)/articles/components/articles/ArticlesList.tsx
@@ -53,74 +53,87 @@ export default function ArticlesList({ dic, blogs, isLoadingBlogs }: Props) {
       </li>
      ))}
     {!isLoadingBlogs &&
-     blogs.map((item) => (
-      <li key={item.id}>
-       <Link
-        href={`/articles/${item.id}?${encodeURI(item.header)}`}
-        style={{
-         transition: 'transform 0.5s ease',
-        }}
-        className='h-full flex flex-col border border-neutral-300 dark:border-neutral-700 bg-background rounded-lg text-foreground hover:scale-105'
-       >
-        <div className='p-2'>
-         <div className='relative after:content-* after:absolute after:inset-0 after:bg-black/10 dark:after:bg-black/20 h-[16rem]'>
-          <ImageWrapper
-           wrapper={{
-            className: 'w-full h-full',
-           }}
-           img={{
-            className: 'w-full h-full object-cover object-center rounded-lg',
-            alt: 'aritcle-imgage',
-            src:
-             (process.env.NEXT_PUBLIC_BASE_URL || '') + item.imageUrl ||
-             '/services/security-camera-installation.jpg',
-           }}
-          />
-         </div>
-        </div>
-        <div className='px-4 py-2 pb-4 flex-grow flex flex-col'>
-         <div className='mb-1 flex'>
-          <div className='text-primary-dark font-medium'>
-           <span className='text-[0.7rem]'>
-            {dateFormatter.format(new Date(item.createDateTimeOffset))}
-           </span>
+     blogs.map((item) => {
+      const articleLink = `/articles/${item.id}?${encodeURI(item.header)}`;
+      return (
+       <li key={item.id}>
+        <Link
+         href={articleLink}
+         style={{
+          transition: 'transform 0.5s ease',
+         }}
+         className='h-full flex flex-col border border-neutral-300 dark:border-neutral-700 bg-background rounded-lg text-foreground hover:scale-105'
+        >
+         <div className='p-2'>
+          <div className='relative after:content-* after:absolute after:inset-0 after:bg-black/10 dark:after:bg-black/20 h-[16rem]'>
+           <ImageWrapper
+            wrapper={{
+             className: 'w-full h-full',
+            }}
+            img={{
+             className: 'w-full h-full object-cover object-center rounded-lg',
+             alt: 'aritcle-imgage',
+             src:
+              (process.env.NEXT_PUBLIC_BASE_URL || '') + item.imageUrl ||
+              '/services/security-camera-installation.jpg',
+            }}
+           />
           </div>
          </div>
-         <h3 className='text-lg font-medium text-secondary mb-3'>
-          {item.header}
-         </h3>
-         <p className='mb-6 leading-6 flex-grow'>
-          {item.description.length > 100
-           ? item.description.slice(0, 100) + '...'
-           : item.description}
-         </p>
-         <div className='flex justify-between items-center'>
-          <div className='flex gap-1'>
-           <IconButton
-            color='primary'
-            className='!bg-sky-300/20 !dark:bg-sky-700/20'
-           >
-            <ShareOutlinedIcon />
-           </IconButton>
-           <IconButton
-            color='error'
-            className='!bg-red-300/20 !dark:bg-red-700/20'
-           >
-            <FavoriteBorderOutlinedIcon />
-           </IconButton>
-          </div>
-          <GradientButton>
-           <div className='flex gap-3 items-center'>
-            <span>{dic.continue as string}</span>
-            <VisibilityIcon />
+         <div className='px-4 py-2 pb-4 flex-grow flex flex-col'>
+          <div className='mb-1 flex'>
+           <div className='text-primary-dark font-medium'>
+            <span className='text-[0.7rem]'>
+             {dateFormatter.format(new Date(item.createDateTimeOffset))}
+            </span>
            </div>
-          </GradientButton>
+          </div>
+          <h3 className='text-lg font-medium text-secondary mb-3'>
+           {item.header}
+          </h3>
+          <p className='mb-6 leading-6 flex-grow'>
+           {item.description.length > 100
+            ? item.description.slice(0, 100) + '...'
+            : item.description}
+          </p>
+          <div className='flex justify-between items-center'>
+           <div className='flex gap-1'>
+            <IconButton
+             color='primary'
+             className='!bg-sky-300/20 !dark:bg-sky-700/20'
+             onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              if (!navigator.share) return;
+              navigator.share({
+                title: item.header,
+                text: item.description,
+                url: articleLink,
+              });
+            }}  
+            >
+             <ShareOutlinedIcon />
+            </IconButton>
+            <IconButton
+             color='error'
+             className='!bg-red-300/20 !dark:bg-red-700/20'
+            >
+             <FavoriteBorderOutlinedIcon />
+            </IconButton>
+           </div>
+           <GradientButton>
+            <div className='flex gap-3 items-center'>
+             <span>{dic.continue as string}</span>
+             <VisibilityIcon />
+            </div>
+           </GradientButton>
+          </div>
          </div>
-        </div>
-        <div></div>
-       </Link>
-      </li>
-     ))}
+         <div></div>
+        </Link>
+       </li>
+      );
+     })}
    </ul>
   </div>
  );
