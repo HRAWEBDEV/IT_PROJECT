661b2d67b2825d10c2c4c4e4caab10a3f08026b0
hamidwise1 <hrawebdev@gmail.com>
2025-05-26 21:05:19 +0330

    update search projects and articles

--- app/[locale]/(website)/components/Search/Search.tsx
+++ app/[locale]/(website)/components/Search/Search.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { FormEvent } from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
@@ -10,6 +11,13 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import Link from 'next/link';
 import { useAppMonitorConfig } from '@/services/app-monitor/appMonitor';
 import { type Dic, type WithDictionary } from '@/localization/locales';
+import { getBlogs, getProjects } from '@/services/api-actions/globalApiActions';
+import { useDebounceValue } from '@/hooks/useDebounceValue';
+import { useQuery } from '@tanstack/react-query';
+import { useAppConfig } from '@/services/app-config/appConfig';
+import ImageWrapper from '@/components/ImageWrapper';
+import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
 
 type Props = WithDictionary & {
  onClose: () => void;
@@ -18,6 +26,192 @@ type Props = WithDictionary & {
 
 export default function Search({ dic, onClose, isOpen }: Props) {
  const { isLargeDevice } = useAppMonitorConfig();
+ const { locale } = useAppConfig();
+ const [searchText, setSearchText] = useState('');
+ const debouncedSearchText = useDebounceValue(searchText, 500);
+
+ const {
+  data: blogs,
+  isLoading: isLoadingBlogs,
+  isSuccess: isSuccessBlogs,
+ } = useQuery({
+  enabled: Boolean(debouncedSearchText),
+  queryKey: ['search', 'articles', debouncedSearchText],
+  queryFn() {
+   return getBlogs({
+    locale,
+    blogStateID: 2,
+    pagination: {
+     offset: 1,
+     limit: 5,
+    },
+    searchText: debouncedSearchText,
+   }).then((res) => res.data.payload.Blogs);
+  },
+ });
+
+ const {
+  data: projects,
+  isLoading: isLoadingProjects,
+  isSuccess: isSuccessProjects,
+ } = useQuery({
+  enabled: Boolean(debouncedSearchText),
+  queryKey: ['search', 'projects', debouncedSearchText],
+  queryFn() {
+   return getProjects({
+    locale,
+    projectStateID: 2,
+    pagination: {
+     offset: 1,
+     limit: 5,
+    },
+    searchText: debouncedSearchText,
+    showForCards: false,
+   }).then((res) => res.data.payload.Projects);
+  },
+ });
+
+ let searchContent = <div></div>;
+ let resultProjects = (
+  <div className='text-center'>{dic.noItemFound as string}</div>
+ );
+ let resultBlogs = (
+  <div className='text-center'>{dic.noItemFound as string}</div>
+ );
+
+ if (isSuccessBlogs) {
+  resultBlogs = (
+   <ul className='grid gap-2'>
+    {!!blogs.rows.length ? (
+     blogs.rows.map((item) => (
+      <li
+       key={item.id}
+       className='transition-colors p-2 rounded-lg hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-900 dark:focus:bg-neutral-900'
+      >
+       <Link
+        href={`/articles/${item.id}?name=${item.header}`}
+        className='flex flex-col md:flex-row md:gap-4'
+       >
+        <div className='shrink-0 mb-2 h-[12rem] md:h-[9rem] md:w-[9rem] rounded-lg overflow-hidden'>
+         <ImageWrapper
+          img={{
+           className:
+            'h-full w-full object-cover object-center border border-neutral-300 dark:border-neutral-700 rounded-lg',
+           src: `${process.env.NEXT_PUBLIC_BASE_URL}${item.imageUrl}`,
+           alt: 'search image',
+          }}
+          wrapper={{
+           className: 'h-full w-full',
+          }}
+         />
+        </div>
+        <div className='flex-grow'>
+         <h3 className='text-base font-medium text-secondary mb-1'>
+          {item.header}
+         </h3>
+         <p className='text-xs'>{item.description}</p>
+        </div>
+       </Link>
+      </li>
+     ))
+    ) : (
+     <div className='text-center'>{dic.noItemFound as string}</div>
+    )}
+   </ul>
+  );
+ }
+ if (isSuccessProjects) {
+  resultProjects = (
+   <ul className='grid gap-2'>
+    {!!projects.rows.length ? (
+     projects.rows.map((item) => (
+      <li
+       key={item.id}
+       className='transition-colors p-2 rounded-lg hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-900 dark:focus:bg-neutral-900'
+      >
+       <Link
+        href={`/projects/${item.id}?name=${item.header}`}
+        className='flex flex-col md:flex-row md:gap-4'
+       >
+        <div className='shrink-0 mb-2 lg:mb-0 h-[12rem] md:h-[9rem] md:w-[9rem] rounded-lg overflow-hidden'>
+         <ImageWrapper
+          img={{
+           className:
+            'h-full w-full object-cover object-center border border-neutral-300 dark:border-neutral-700 rounded-lg',
+           src: `${process.env.NEXT_PUBLIC_BASE_URL}${item.imageUrl}`,
+           alt: 'search image',
+          }}
+          wrapper={{
+           className: 'h-full w-full',
+          }}
+         />
+        </div>
+        <div className='flex-grow'>
+         <h3 className='text-base font-medium text-secondary mb-2'>
+          {item.header}
+         </h3>
+         <p className='text-xs'>{item.description}</p>
+        </div>
+       </Link>
+      </li>
+     ))
+    ) : (
+     <div className='text-center'>{dic.noItemFound as string}</div>
+    )}
+   </ul>
+  );
+ }
+
+ if (isLoadingProjects || isLoadingBlogs) {
+  searchContent = (
+   <div className='flex justify-center items-center h-[8rem]'>
+    <CircularProgress />
+   </div>
+  );
+ }
+
+ if (debouncedSearchText && !isLoadingProjects && !isLoadingBlogs) {
+  searchContent = (
+   <section className='p-4 pt-0'>
+    <article className='mb-6'>
+     <div className='flex gap-4 items-center mb-4'>
+      <h3 className='text-primary-dark text-base font-medium'>
+       {(dic.search as Dic).articles as string}
+      </h3>
+      <div className='flex-grow h-[1px] bg-primary-dark'></div>
+      <Button
+       LinkComponent={Link}
+       href={`/articles?searchText=${searchText}`}
+       color='warning'
+      >
+       {(dic.search as Dic).showAll as string}
+      </Button>
+     </div>
+     {resultBlogs}
+    </article>
+    <article className='mb-6'>
+     <div className='flex gap-4 items-center mb-4'>
+      <h3 className='text-primary-dark text-base font-medium'>
+       {(dic.search as Dic).projects as string}
+      </h3>
+      <div className='flex-grow h-[1px] bg-primary-dark'></div>
+      <Button
+       LinkComponent={Link}
+       href={`/projects?searchText=${searchText}`}
+       color='warning'
+      >
+       {(dic.search as Dic).showAll as string}
+      </Button>
+     </div>
+     {resultProjects}
+    </article>
+   </section>
+  );
+ }
+
+ useEffect(() => {
+  setSearchText('');
+ }, [isOpen]);
  return (
   <Dialog
    className='[&_.MuiPaper-root]:backdrop-blur-sm'
@@ -52,8 +246,11 @@ export default function Search({ dic, onClose, isOpen }: Props) {
     </div>
    </DialogTitle>
    <DialogContent dividers>
-    <div className='p-4 sticky top-0 bg-background'>
+    <div className='p-4 sticky top-0 bg-background z-10'>
      <TextField
+      name='search'
+      value={searchText}
+      onChange={(e) => setSearchText(e.target.value)}
       sx={{
        '& .MuiInputBase-root': {
         borderRadius: '0.8rem',
@@ -75,44 +272,7 @@ export default function Search({ dic, onClose, isOpen }: Props) {
       fullWidth
      />
     </div>
-    <section className='p-4 pt-0'>
-     {['خدمــات', 'پروژه‌هـــا'].map((item) => (
-      <article key={item} className='mb-6'>
-       <div className='flex gap-4 items-center mb-4'>
-        <h3 className='text-primary-dark text-base font-medium'>{item}</h3>
-        <div className='flex-grow h-[1px] bg-primary-dark'></div>
-       </div>
-       <ul className='grid gap-2'>
-        {[1, 2, 3, 4].map((item) => (
-         <li
-          key={item}
-          className='transition-colors p-2 rounded-lg hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-900 dark:focus:bg-neutral-900'
-         >
-          <Link href={'#'} className='flex flex-col md:flex-row md:gap-4'>
-           <div className='shrink-0 mb-2 h-[9rem] md:h-[6rem] md:w-[8rem] rounded-lg overflow-hidden'>
-            <img
-             className='h-full w-full object-cover object-center'
-             src='/services/security-camera-installation.jpg'
-             alt='search image'
-            />
-           </div>
-           <div className='flex-grow'>
-            <h3 className='text-base font-medium text-secondary mb-2'>
-             تفاوت پشتیبانی شبکه فیزیکی و نرم افزاری
-            </h3>
-            <p className='text-xs'>
-             در دنیای پرشتاب فناوری اطلاعات، پشتیبانی سیستم‌ها یکی از عوامل
-             کلیدی در دنیای پرشتاب فناوری اطلاعات، پشتیبانی سیستم‌ها یکی از
-             عوامل کلیدی برای حفظ ....
-            </p>
-           </div>
-          </Link>
-         </li>
-        ))}
-       </ul>
-      </article>
-     ))}
-    </section>
+    {searchContent}
    </DialogContent>
   </Dialog>
  );
